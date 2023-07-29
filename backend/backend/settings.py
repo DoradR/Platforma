e2b7dcebad4cc149b6ebf99c9f64fadf75d184e6
DoradR <dominik.radziszewski99@gmail.com>
2023-07-29 15:15:24 +0200

    Newest progres for changing device

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-=eqxd1rew*hxtry+!l!_k=!mcyubpuhhax*iy$opyl3ap%zk=r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.1.156',]
 
 
 # Application definition
--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,7 @@
 const config = {
     development: {
-      backendUrl: 'http://localhost:8000',
+      // backendUrl: 'http://localhost:8000',
+      backendUrl: 'http://192.168.1.156:8000',
     },
     production: {
       backendUrl: 'http://backend.example.com',
